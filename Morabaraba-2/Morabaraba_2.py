4e8009607adaac39d32a95450d959be58277e545
Dane Horn <g16H0473@campus.ru.ac.za>
2018-03-22 15:08:10 +0200

    Add Morabaraba skeleton class

--- Morabaraba-2/Morabaraba_2.py
+++ Morabaraba-2/Morabaraba_2.py
@@ -1,1 +1,7 @@
+myDict = {}
 
+myDict[1] = "one"
+myDict[2] = "two"
+
+for k, v in myDict.items():
+    print (f"{k}:{v}")
---
+++ Morabaraba-2/Morabaraba_OO.py
@@ -0,0 +1,15 @@
+class MorabarabaGame:
+    class Board:
+        state = []
+        def __init__(self):
+            #initialise state
+            self.state = []
+
+    class Cell:
+        state = ""
+        pos = ""
+        def __init__(self):
+            self.state = ""
+            self.pos = ""
+
+    
